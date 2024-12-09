6ad9d0043d615fecbec8de4a18f7ebd203eb9021
Meghana <meghanamanchal@gmail.com>
2024-12-09 12:02:02 +0530

    its a second commit

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 
 let userChoice
+let computerChoice
+let result
+
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e)=>{
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random()*3)
-    console.log(randomNumber)
+    const randomNumber = Math.floor(Math.random()*3) + 1
+    if(randomNumber == 1){
+        computerChoice = 'rock'
+    }
+    if(randomNumber == 2){
+        computerChoice = 'scissors'
+    }
+    if(randomNumber == 3){
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+      } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') ||
+        (userChoice === 'paper' && computerChoice === 'rock')
+      ) {
+        result = "You win!";
+      } else {
+        result = "You lose!";
+      }
+      resultDisplay.innerHTML = result
 }
